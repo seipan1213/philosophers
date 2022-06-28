03a0f1c8e79a437914456e58e816d9ef8edb1b67
seipan1213 <seiju1213@gmail.com>
2022-06-28 09:47:15 +0900

    哲学者 初期化 実装

--- philo/includes/philo.h
+++ philo/includes/philo.h
@@ -1,11 +1,39 @@
 #ifndef PHILO_H
 #define PHILO_H
 
+#include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
 #include <unistd.h>
-
+#include <pthread.h>
 #define ARG_ERR "ARGERR"
+#define MALLOC_ERR "MALLOCERR"
+#define MUTEX_ERR "MUTEXERR"
+
+typedef struct s_men
+{
+	pthread_mutex_t *left;
+	pthread_mutex_t *right;
+	pthread_mutex_t *died;
+	pthread_mutex_t *eat;
+	int *eat_cnt;
+} t_men;
+
+typedef struct s_philo
+{
+	int number_of_philosophers;
+	int time_to_die;
+	int time_to_eat;
+	int time_to_sleep;
+	int number_of_times_each_philosopher_must_eat;
+	pthread_mutex_t *forks;
+	t_men *men;
+	pthread_mutex_t died;
+	pthread_mutex_t eat;
+	int eat_cnt;
+} t_philo;
+
+int ph_free(t_philo *ph, int ret);
 
 int put_err(char *str);
 size_t ft_strlen(char *str);
--- philo/srcs/main.c
+++ philo/srcs/main.c
@@ -1,19 +1,99 @@
 #include "philo.h"
 
-bool input_check(int argc, char **argv)
+bool ph_args_init(int argc, char **argv, t_philo *ph)
 {
-	(void)argv;
-	if (argc != 5)
-		return (false);
+	if (argc != 5 && argc != 6)
+		return (put_err(ARG_ERR));
+	ph->number_of_philosophers = ft_atoi(argv[1]);
+	ph->time_to_die = ft_atoi(argv[2]);
+	ph->time_to_eat = ft_atoi(argv[3]);
+	ph->time_to_sleep = ft_atoi(argv[4]);
+	if (argc == 6)
+		ph->number_of_times_each_philosopher_must_eat = ft_atoi(argv[5]);
 	// TODO: argv 数値判定
-	return (true);
+	return (false);
 }
 
+bool ph_men_init(t_philo *ph)
+{
+	int num;
+	int i;
+
+	i = -1;
+	num = ph->number_of_philosophers;
+	ph->men = malloc(sizeof(t_men) * num);
+	if (ph->men == NULL)
+		return (put_err(MALLOC_ERR));
+	while (++i < num)
+	{
+		ph->men[i].right = &ph->forks[i];
+		ph->men[i].left = &ph->forks[(i + num - 1) % num];
+		ph->men[i].died = &ph->died;
+		ph->men[i].eat = &ph->eat;
+		ph->men[i].eat_cnt = &ph->eat_cnt;
+	}
+	return (false);
+}
+
+bool ph_main_init(t_philo *ph)
+{
+	int num;
+	int i;
+
+	num = ph->number_of_philosophers;
+	ph->forks = malloc((sizeof(pthread_mutex_t) * num));
+	if (ph->forks == NULL)
+		return (put_err(MALLOC_ERR));
+	i = -1;
+	while (++i < num)
+	{
+		if (pthread_mutex_init(&ph->forks[i], NULL))
+			return (put_err(MUTEX_ERR));
+	}
+	if (pthread_mutex_init(&ph->died, NULL))
+		return (put_err(MUTEX_ERR));
+	if (pthread_mutex_init(&ph->eat, NULL))
+		return (put_err(MUTEX_ERR));
+	return (false);
+}
+
+bool ph_init(int argc, char **argv, t_philo *ph)
+{
+	ft_bzero(ph, sizeof(t_philo));
+	if (ph_args_init(argc, argv, ph))
+		return (true);
+	if (ph_main_init(ph))
+		return (ph_free(ph, true));
+	if (ph_men_init(ph))
+		return (ph_free(ph, true));
+	return (false);
+}
+
+int ph_free(t_philo *ph, int ret)
+{
+	if (ph->forks != NULL)
+		free(ph->forks);
+	if (ph->men != NULL)
+		free(ph->men);
+	return (ret);
+}
+void ph_mutex_destoy(t_philo *ph)
+{
+	int i;
+
+	i = -1;
+	while (++i < ph->number_of_philosophers)
+		pthread_mutex_destroy(&ph->forks[i]);
+	pthread_mutex_destroy(&ph->died);
+	pthread_mutex_destroy(&ph->eat);
+}
 int main(int argc, char **argv)
 {
-	if (!input_check(argc, argv))
-	{
-		put_endl_fd(ARG_ERR, STDERR_FILENO);
+	t_philo ph;
+
+	if (ph_init(argc, argv, &ph))
 		return (1);
-	}
+	printf("%d  %d  %d  %d  %d\n", ph.number_of_philosophers, ph.time_to_die, ph.time_to_eat, ph.time_to_sleep, ph.number_of_times_each_philosopher_must_eat);
+	ph_mutex_destoy(&ph);
+	return (ph_free(&ph, 0));
 }
