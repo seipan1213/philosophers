e1e36d60b308cdc148753f79aef3568f8aedec32
seipan1213 <seiju1213@gmail.com>
2022-07-10 12:52:25 +0900

    初期待機 偶数に変更

--- philo/srcs/ph_work.c
+++ philo/srcs/ph_work.c
@@ -6,7 +6,7 @@
 /*   By: sehattor <sehattor@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/09 22:38:25 by sehattor          #+#    #+#             */
-/*   Updated: 2022/07/10 12:33:10 by sehattor         ###   ########.fr       */
+/*   Updated: 2022/07/10 12:51:52 by sehattor         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -66,7 +66,7 @@ void	*ph_work(void *arg)
 	man = (t_man *)arg;
 	get_is_fin(man);
 	set_last_eat_time(man, false);
-	if (man->id % 2 == 1)
+	if (man->id % 2 == 0)
 		man_sleep(man->time_to_eat / 2, man);
 	while (!get_is_fin(man))
 	{
